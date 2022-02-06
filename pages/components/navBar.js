5cdbbd08ebee454d121903bed1e86660f6cedca1
kimutai-codes <allankimutaikoech@gmail.com>
2022-02-06 17:58:58 +0300

    navbar changes

--- pages/components/navBar.js
+++ pages/components/navBar.js
@@ -4,38 +4,68 @@ import React from 'react';
 const Navbar = () => {
 	return (
 		<div className='navbar'>
+			{/* TODO the first list items will be buttons and the second will only show when the first is clicked */}
+			{/* how to select second item in css */}
 			<ul>
-				{/* //abouts page */}
-				{/* <li>
-            <Link href='/about'><a>About Us</a></Link>
-          </li> */}
-				{/* Departments */}
-				{/* <li className='Department'>Department</li>
-          <li>
-            <Link href='/Kindom-Generation'>Kingdom Generation</Link>
-          </li>
-          <li>
-            <Link href='/Youths'>Youth</Link>
-          </li> */}
 				<li>About</li>
+
+				{/* btn for departnmets */}
 				<li>
 					Departments
 					<ul>
-						<li>Kindom Generation</li>
-						<li>Youth</li>
-						<li>DOF</li>
-						<li>Eagles</li>
+						<li>
+							<Link href='/kingdom-generation'>
+								<a>Kindom Generation</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='/youth'>
+								<a>Youth</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='DOF'>
+								<a>DOF</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='eagles'>
+								<a>Eagles</a>
+							</Link>
+						</li>
 					</ul>
 				</li>
+
+				{/* another btn for ministries */}
 				<li>
 					Ministries
 					<ul>
-						<li>Ushering</li>
-						<li>Music</li>
-						<li>Hospitality</li>
+						<li>
+							<Link href='/Ushering'>
+								<a>Ushering</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='/Music'>
+								<a>Music</a>
+							</Link>
+						</li>
+						<li>
+							<Link href='/Hospitality'>
+								<a>Hospitality</a>
+							</Link>
+						</li>
 					</ul>
 				</li>
 			</ul>
+			<style jsx>{`
+				.navbar {
+					color: red;
+					display: flex;
+					flex-direction: row;
+					width: 100%;
+				}
+			`}</style>
 		</div>
 	);
 };
