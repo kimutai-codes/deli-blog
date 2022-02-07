f91fb59b609e50ea3c8e7f939740bb7d094aafda
kimutai-codes <allankimutaikoech@gmail.com>
2022-02-07 17:00:42 +0300

    t-t

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div className='footer'>yeh</div>;
+	return (
+		<footer>
+			<p>This is the footer</p>
+			<style jsx>{`
+				footer {
+					background-color: grey;
+					position: absolute;
+          bottom: 0;
+					width: 100%;
+				}
+			`}</style>
+		</footer>
+	);
 };
 
 export default Footer;
