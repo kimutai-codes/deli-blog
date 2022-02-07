7f5f72051366e156d32deb7874bd89c736a9b7eb
kimutai-codes <allankimutaikoech@gmail.com>
2022-02-07 17:36:58 +0300

    decoupled

--- pages/components/navBar.js
+++ pages/components/navBar.js
@@ -19,86 +19,86 @@ const Navbar = () => {
 		<div className='navbar-div'>
 			{/* TODO the first list items will be buttons and the second will only show when the first is clicked */}
 			{/* how to select second item in css */}
-			<ul className='navbar'>
-				<li>
-					<Link href='/'>
-						<a>
-							<Image
-								src='/logo.png'
-								alt='logo'
-								width='30px'
-								height='30px'
-							/>
-						</a>
-					</Link>
-				</li>
-
-				<li>About</li>
-
-				{/* btn for departnmets */}
-				<li
-					className={`departments btn ${dropDown ? 'dropped' : ''}`}
-					onClick={dropClick}
-				>
-					Departments
-					<ul>
-						<li>
-							<Link href='/kingdom-generation'>
-								<a>Kindom Generation</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/youth'>
-								<a>Youth</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='DOF'>
-								<a>DOF</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='eagles'>
-								<a>Eagles</a>
-							</Link>
-						</li>
-					</ul>
-				</li>
-
-				{/* another btn for ministries */}
-				<li
-					className={`ministries btn ${minDrop ? 'mindropped' : ''}`}
-					onClick={dropMin}
-				>
-					Ministries
-					<ul>
-						<li>
-							<Link href='/Ushering'>
-								<a>Ushering</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/Music'>
-								<a>Music</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/Hospitality'>
-								<a>Hospitality</a>
-							</Link>
-						</li>
-					</ul>
-				</li>
-				{/* hambugger menu */}
-				<li className='menu'>
+			<Link href='/' className='logo'>
+				<a>
 					<Image
-						src='/hamburger-menu-svgrepo-com.svg'
+						src='/logo.png'
 						alt='logo'
 						width='30px'
 						height='30px'
 					/>
-				</li>
-			</ul>
+				</a>
+			</Link>
+
+			<div>About</div>
+
+			{/* btn for departnmets */}
+			<div>
+				<button
+					className={`departments btn ${dropDown ? 'dropped' : ''}`}
+					onClick={dropClick}
+				>
+					Departments
+				</button>
+				<ul>
+					<li>
+						<Link href='/kingdom-generation'>
+							<a>Kindom Generation</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/youth'>
+							<a>Youth</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='DOF'>
+							<a>DOF</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='eagles'>
+							<a>Eagles</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+
+			{/* another btn for ministries */}
+			<div>
+				<button
+					className={`ministries btn ${minDrop ? 'mindropped' : ''}`}
+					onClick={dropMin}
+				>
+					Ministries
+				</button>
+				<ul>
+					<li>
+						<Link href='/Ushering'>
+							<a>Ushering</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/Music'>
+							<a>Music</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/Hospitality'>
+							<a>Hospitality</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+			{/* hambugger menu */}
+			<div>
+				<Image
+					src='/hamburger-menu-svgrepo-com.svg'
+					alt='logo'
+					width='30px'
+					height='30px'
+				/>
+			</div>
 			<style jsx>{``}</style>
 		</div>
 	);
