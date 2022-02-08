ff658486cd6d22d43247c09ab24445d16ead4dd1
kimutai-codes <allankimutaikoech@gmail.com>
2022-02-08 11:54:44 +0300

    big screen at least

--- pages/components/navBar.js
+++ pages/components/navBar.js
@@ -16,53 +16,51 @@ const Navbar = () => {
 		setDropDown(!dropDown);
 	};
 	return (
-		<div className='navbar-div'>
+		<div className='header'>
+			{/* nav link => home */}
+			<Link href='/' className='logo'>
+				<a>
+					<Image
+						src='/logo.png'
+						alt='logo'
+						width='30px'
+						height='30px'
+					/>
+				</a>
+			</Link>
 			{/* TODO the first list items will be buttons and the second will only show when the first is clicked */}
 			{/* how to select second item in css */}
 			<ul className='navbar'>
 				<li>
-					<Link href='/'>
-						<a>
-							<Image
-								src='/logo.png'
-								alt='logo'
-								width='30px'
-								height='30px'
-							/>
-						</a>
+					<Link href='/about'>
+						<a>About</a>
 					</Link>
 				</li>
 
-				<li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-
 				{/* btn for departnmets */}
-				<li
-					className={`departments btn ${dropDown ? 'dropped' : ''}`}
-					onClick={dropClick}
-				>
-					Departments
+				<li>
+					Departments +
 					<ul>
 						<li>
 							<Link href='/kingdom-generation'>
-								<a>Kindom Generation</a>
+								<a>Kingdom Generation</a>
 							</Link>
 						</li>
+
 						<li>
 							<Link href='/youth'>
 								<a>Youth</a>
 							</Link>
 						</li>
+
 						<li>
-							<Link href='DOF'>
-								<a>DOF</a>
+							<Link href='/DOF'>
+								<a>D.O.F</a>
 							</Link>
 						</li>
+
 						<li>
-							<Link href='eagles'>
+							<Link href='/eagles'>
 								<a>Eagles</a>
 							</Link>
 						</li>
@@ -70,11 +68,8 @@ const Navbar = () => {
 				</li>
 
 				{/* another btn for ministries */}
-				<li
-					className={`ministries btn ${minDrop ? 'mindropped' : ''}`}
-					onClick={dropMin}
-				>
-					Ministries
+				<li>
+					Ministries +
 					<ul>
 						<li>
 							<Link href='/Ushering'>
@@ -93,46 +88,57 @@ const Navbar = () => {
 						</li>
 					</ul>
 				</li>
-				{/* hambugger menu */}
-				<li className='menu'>
-					<Image
-						src='/hamburger-menu-svgrepo-com.svg'
-						alt='logo'
-						width='30px'
-						height='30px'
-					/>
-				</li>
 			</ul>
+			{/* hambugger menu */}
+			<Image
+				src='/hamburger-menu-svgrepo-com.svg'
+				alt='logo'
+				width='30px'
+				height='30px'
+			/>
 			<style jsx>{`
-				.navbar {
+				.header {
+					background-color: rgb(240, 234, 234);
+					text-transform: capitalize;
+					color: rgb(158, 24, 24);
 					display: flex;
-					flex-direction: row;
+					align-items: center;
 					justify-content: space-between;
 				}
 
-				.btn a {
-					display: none;
+				.navbar > li {
+					margin: 0 80px;
 				}
-
 				.navbar li {
-					padding: 5px;
-					text-align: center;
-					color: saddlebrown;
+					position: relative;
+					float: left;
+					padding: 0 5px;
+					font-size: 20px;
 				}
-				.menu {
-					display: none;
+				.navbar ul li a {
+					display: block;
+					padding: 10px;
+				}
+				.navbar ul li a:hover {
+					background-color: grey;
 				}
 
-				@media(max-width:600px) {
-          .menu{
-            display: block;
-          }
-					.dropped a {
-						display: inline;
-					}
-					.mindropped a {
-						display: inline;
-					}
+				.navbar li ul {
+					display: none;
+					position: absolute;
+					width: 200px;
+					float: left;
+					background-color: white;
+				}
+
+				.navbar ul li {
+					width: 100;
+					border-top: 1px solid grey;
+				}
+
+				.navbar li:focus-within > ul,
+				.navbar li:hover > ul {
+					display: initial;
 				}
 			`}</style>
 		</div>
